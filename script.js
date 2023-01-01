380f56305668612d5cd63f2cef9e00f58d6d0ac9
henrypeters <meethenry09@gmail.com>
2023-01-01 19:11:18 +0100

    chore: added comment to functions

--- script.js
+++ script.js
@@ -15,13 +15,15 @@ currentAge.addEventListener("keyup", getAge)
 
 
 
-
+// ES6 function syntax
 const toNum = (num) => Number.parseInt(num, 10)
 
+// ES5 function syntax
 function toNum2 (num) {
     return Number.parseInt(num, 10)
 }
 
+// calcute the sum of 2 numbers
 const calculateNum = (x, y) => {
    const sum =  Number.parseInt(x, 10) + Number.parseInt(y, 10)
    return sum;
