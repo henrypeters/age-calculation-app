787aac5ecbf73fa5c2bdfba9b27af607bd426643
henrypeters <meethenry09@gmail.com>
2023-01-02 13:23:21 +0100

    fix: remove NaN

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const futureAge = document.getElementById("age");
 
 
 function getFutureAge(value) {
-    // return Number.parseInt(value, 10) + 1;
-    // if (value >= )
+    if (value.length >= 1) {
+        return Number.parseInt(value, 10) + 1;
+    }else {
+        return 0;
+    }
 }
 
 function getAge() {
@@ -18,14 +21,12 @@ currentAge.addEventListener("keyup", getAge)
 // ES6 function syntax
 const toNum = (num) => Number.parseInt(num, 10)
 
-// ES5 function syntax
-function toNum2 (num) {
-    return Number.parseInt(num, 10)
-}
+
 
 // calcute the sum of 2 numbers
 const calculateNum = (x, y) => {
-   const sum =  Number.parseInt(x, 10) + Number.parseInt(y, 10)
-   return sum;
+    const sum = toNum(x) + toNum(y)
+    console.log(`sum: ${sum}`)
+    return sum;
 
 }
